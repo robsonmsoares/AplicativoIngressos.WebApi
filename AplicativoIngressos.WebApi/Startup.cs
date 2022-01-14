f4258def5785665de91990cc780030ad98e73ff8
Robson Mosena Soares <robsonmsoares88@gmail.com>
2022-01-13 22:35:26 -0300

    Alterado o nome da solucao

--- AplicativoIngressos.WebApi/Startup.cs
+++ AplicativoIngressos.WebApi/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Hosting;
 using AplicacaoIngressos.WebApi.Infraestrutura;
 using Microsoft.EntityFrameworkCore;
 
-namespace AplicativoIngressos.WebApi
+namespace AplicacaoIngressos.WebApi
 {
     public class Startup
     {
